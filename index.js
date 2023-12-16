5c22e740511549b96f86b8f6bbae4182ae327057
James <james.kariuki2@student.moringaschool.com>
2023-12-16 22:56:05 +0300

    challenge1

---
+++ index.js
@@ -0,0 +1,22 @@
+function grade() {
+    if (grade >= 79 && 100 );
+}
+return A;
+
+function grade() {
+    if (grade >= 60 && grade <= 78 )
+    return B;
+}
+
+function grade() {
+    if (grade >= 49 && grade <= 59 )
+    return C;
+}
+function grade() {
+    if (grade >= 40 && grade <= 48)
+    return D;
+}
+function grade() {
+    if (grade <= 40 )
+    return E;
+}
